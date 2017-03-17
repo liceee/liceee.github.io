dc84e10855c0636fb95ec29a3db05f649cfcb616
lee <lee@LEE-PC>
2017-03-17 18:24:25 +0800

    Update 3.0

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function() {
+	/*
+	 * Add Section links
+	 */
+	var section = 0;
+	$('.single > .content > h6:not(.title)').append(function() {
+		section++;
+		return " <a name=\"section" + section + "\" class=\"section\" href=\"#section" + section + "\">&#123;&nbsp;section&nbsp;" + section + "&nbsp;&#125;</a>"
+	});
+});
